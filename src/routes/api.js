eec3a163c97af971a64def9097fd55de34e27d9a
Greg Brisebois <gregoryjjb@gmail.com>
2018-12-21 14:34:16 -0800

    Config editable through API

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const shows = require('./shows');
 const play = require('./play');
+const config = require('./config');
 
 const api = express.Router();
 
 api.use('/shows', shows);
 api.use('/play', play);
+api.use('/config', config);
 
 api.get('/', (req, res) => {
     res.send("This is the API");
---
+++ src/routes/config.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const config = require('../config');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json(config.get());
+});
+
+router.put('/', (req, res) => {
+    const newConfig = req.body;
+    
+    config.save(newConfig);
+    
+    res.end();
+})
+
+module.exports = router;
